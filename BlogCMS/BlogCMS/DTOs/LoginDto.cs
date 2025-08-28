0a90422ec0c4bb7ec09cdd1e42326104f7310eaa
Bheema08 <devabopparaju@gmail.com>
2025-08-28 11:04:23 +0530

    code

---
+++ BlogCMS/BlogCMS/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿// DTOs/LoginDto.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogCMS.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ BlogCMS/BlogCMS/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿// Data/AppDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using BlogCMS.Models;
+
+namespace BlogCMS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure many-to-many relationship
+            modelBuilder.Entity<BlogPost>()
+                .HasMany(p => p.Categories)
+                .WithMany(c => c.BlogPosts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "BlogPostCategories",
+                    j => j.HasOne<Category>().WithMany().HasForeignKey("CategoryId"),
+                    j => j.HasOne<BlogPost>().WithMany().HasForeignKey("BlogPostId"),
+                    j => j.HasKey("BlogPostId", "CategoryId"));
+        }
+    }
+}
